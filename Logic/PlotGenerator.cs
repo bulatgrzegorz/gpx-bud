c1b20755f7c16ed3c1fa20a5b1efd3b24da96bac
Grzegorz.Bulat <Grzegorz.Bulat@GAINCapital.com>
2023-07-28 11:45:29 +0200

    add switch angle/percent

--- Logic/PlotGenerator.cs
+++ Logic/PlotGenerator.cs
@@ -4,76 +4,65 @@ namespace gpxSlopeCalculator.Logic;
 
 public static class PlotGenerator
 {
+    public static double CalculateSlopeGrade(double rise, double run, SlopeGradeType slopeGradeType)
+    {
+        return slopeGradeType switch
+        {
+            SlopeGradeType.Percent => 100 * rise / run,
+            SlopeGradeType.Angle => MathHelper.RadiansToAngle(Math.Atan(rise / run)),
+            _ => throw new NotSupportedException($"Slope grade type: {slopeGradeType} is not supported")
+        };
+    }
+    
     public static string SlopeColorPerGainHex(double slopeAngle)
     {
         var color = SlopeColorPerGain(slopeAngle);
         return $"#{color.R:X2}{color.G:X2}{color.B:X2}";
     }
-    
-    public static string GetAngleLabel(double slopeAngle)
+
+    public static string GetSlopeTypeSuffix(SlopeGradeType type)
     {
-        return slopeAngle switch
+        return type switch
         {
-            _ when slopeAngle < -40 => "<-40",
-            _ when slopeAngle < -35 => "<-35",
-            _ when slopeAngle < -25 => "<-25",
-            _ when slopeAngle < -15 => "<-15",
-            _ when slopeAngle < -5 => "<-5",
-            _ when slopeAngle < 0 => "<0",
-            _ when slopeAngle < 5 => "<5",
-            _ when slopeAngle < 15 => "<15",
-            _ when slopeAngle < 25 => "<25",
-            _ when slopeAngle < 35 => "<35",
+            SlopeGradeType.Angle => "°", 
+            SlopeGradeType.Percent => "%", 
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+    
+    public static string GetSlopeLabel(double slope)
+    {
+        return slope switch
+        {
+            _ when slope < -40 => "<-40",
+            _ when slope < -35 => "<-35",
+            _ when slope < -25 => "<-25",
+            _ when slope < -15 => "<-15",
+            _ when slope < -5 => "<-5",
+            _ when slope < 0 => "<0",
+            _ when slope < 5 => "<5",
+            _ when slope < 15 => "<15",
+            _ when slope < 25 => "<25",
+            _ when slope < 35 => "<35",
             _ => ">35",
         };
     }
 
-    private static Color SlopeColorPerGain(double slopeAngle)
+    private static Color SlopeColorPerGain(double slope)
     {
-        return slopeAngle switch
+        return slope switch
         {
-            _ when slopeAngle < -40 => Color.FromArgb(72, 143, 49),
-            _ when slopeAngle < -35 => Color.FromArgb(63, 150, 104),
-            _ when slopeAngle < -25 => Color.FromArgb(102, 164, 100),
-            _ when slopeAngle < -15 => Color.FromArgb(140, 177, 97),
-            _ when slopeAngle < -5 => Color.FromArgb(179, 189, 97),
-            _ when slopeAngle < 0 => Color.FromArgb(219, 198, 103),
-            _ when slopeAngle < 5 => Color.FromArgb(226, 175, 85),
-            _ when slopeAngle < 15 => Color.FromArgb(230, 150, 75),
-            _ when slopeAngle < 25 => Color.FromArgb(232, 123, 74),
-            _ when slopeAngle < 35 => Color.FromArgb(230, 95, 80),
+            _ when slope < -40 => Color.FromArgb(72, 143, 49),
+            _ when slope < -35 => Color.FromArgb(63, 150, 104),
+            _ when slope < -25 => Color.FromArgb(102, 164, 100),
+            _ when slope < -15 => Color.FromArgb(140, 177, 97),
+            _ when slope < -5 => Color.FromArgb(179, 189, 97),
+            _ when slope < 0 => Color.FromArgb(219, 198, 103),
+            _ when slope < 5 => Color.FromArgb(226, 175, 85),
+            _ when slope < 15 => Color.FromArgb(230, 150, 75),
+            _ when slope < 25 => Color.FromArgb(232, 123, 74),
+            _ when slope < 35 => Color.FromArgb(230, 95, 80),
             _ => Color.FromArgb(222, 66, 91),
         };
     }
-    
-    public static object Config => new
-    {
-        Type = "line",
-        options = new
-        {
-            maintainAspectRatio = false,
-        },
-        Data = new
-        {
-            Datasets = new[]
-            {
-                new {
-                    label = "My First dataset",
-                    backgroundColor = "rgb(255, 99, 132)",
-                    borderColor = "rgb(255, 99, 132)",
-                    data = new [] { 0, 10, 5, 2, 20, 30, 45 },
-                }
-                            
-            },
-            Labels = new[] 
-            {
-                "January",
-                "February",
-                "March",
-                "April",
-                "May",
-                "June"
-            }
-        }
-    };
 }
